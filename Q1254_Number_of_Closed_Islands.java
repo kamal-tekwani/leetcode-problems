ae28e0a38ebfa745fe6d866f2fca9a54cbb7c588
kamal-tekwani <106572666+kamal-tekwani@users.noreply.github.com>
2022-06-02 00:19:39 +0530

    Initial Commit

--- Q1254_Number_of_Closed_Islands.java
+++ Q1254_Number_of_Closed_Islands.java
@@ -13,44 +13,44 @@ public class Q1254_Number_of_Closed_Islands
 	 *  Water - 1
 	 *  Visited - 2
 	 */
-	
+
 	/*
 	 *  Time Complexity : O(m*n)
 	 *  Space Complexity : O(1)
 	 */
-	
+
 	public int closedIsland(int[][] grid)
 	{
 		int count = 0;
-        int row = grid.length;
+		int row = grid.length;
 
-        /*
+		/*
 		 * If row count is zero, 
 		 * then the grid is empty so we will return number of islands as zero.
 		 */
-       
-        if(row == 0)
-        	return count;
-        
-        int column = grid[0].length;
-      
-        /*
-         *  We will be checking for land from the next row and column, 
-         *  i.e land on perimeter cannot form closed island,
-         *  else just access the adjacents nodes.
-         */
-        
-        for(int i=1 ; i<row ; i++)
-        {
-        	for(int j=1 ; j<column ; j++)
-        	{
-        		if(grid[i][j] == 0 && dfs(grid, i, j, row, column))
-        			count++;
-        	}
-        }
-        return count;
-    }
-	
+
+		if(row == 0)
+			return count;
+
+		int column = grid[0].length;
+
+		/*
+		 *  We will be checking for land from the next row and column, 
+		 *  i.e land on perimeter cannot form closed island,
+		 *  else just access the adjacents nodes.
+		 */
+
+		for(int i=1 ; i<row ; i++)
+		{
+			for(int j=1 ; j<column ; j++)
+			{
+				if(grid[i][j] == 0 && dfs(grid, i, j, row, column))
+					count++;
+			}
+		}
+		return count;
+	}
+
 	public boolean dfs(int grid[][],int i,int j,int row,int column)
 	{
 		/*
@@ -59,38 +59,38 @@ public class Q1254_Number_of_Closed_Islands
 		 */
 		if(grid[i][j] == 2 || grid[i][j] == 1)
 			return true;
-		
+
 		/*
 		 * If the land(1) node is on perimeter,
 		 * then it cannot form a closed island.
 		 */
-		
+
 		if(onPerimeter(i, j, row, column))
 			return false;
-		
+
 		// Mark the Node as Visited by assigning value 2
 		grid[i][j] = 2;
-		
+
 		//Top Move
 		boolean top = dfs(grid, i-1, j, row, column);
-		
+
 		// Right Move
 		boolean right = dfs(grid, i, j+1, row, column);
-		
+
 		// Down Move
 		boolean down = dfs(grid, i+1, j, row, column);
-		
+
 		// Left Move
 		boolean left = dfs(grid, i, j-1, row, column);
-		
-		
+
+
 		/* 
 		 * All the adjacents nodes should be surrounded
 		 * by water in order to form a closed island.
-		*/
+		 */
 		return top && right && down && left;
 	}
-	
+
 	public boolean onPerimeter(int i,int j,int row,int column)
 	{
 		// Checking the if the coordinates(i,j) of land (0) lie on the perimeter 
